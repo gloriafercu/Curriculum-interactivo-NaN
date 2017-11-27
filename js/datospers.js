4cd3ae0fa84b8c09b2bcb439901e327e5b3b5ecd
mluena <maluenarod@gmail.com>
2017-11-27 23:41:15 +0100

    adding ids

---
+++ js/datospers.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var photo = document.querySelector('#photo');
+var name = document.querySelector('#name');
+var surnames = document.querySelector('#surnames');
+var email = document.querySelector('#email');
+var phone = document.querySelector('#phone');
+var socialmedia = document.querySelector('#socialmedia');
+var address = document.querySelector('#address');
+var profession = document.querySelector('#profession');
+
+function fillcontactdetails=(){
+	var detail=
+}
+var firstName = prompt('¿Cuál es tu nombre?');
+alert(firstName);
+var surnames = prompt('¿Cuál es tu nombre?');
+alert(firstName);
