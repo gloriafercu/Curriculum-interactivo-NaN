0321624f8eb85c1b44057dd5e05a30dc4d7a41ea
gloriafercu <gloriafercu@gmail.com>
2017-11-29 18:14:14 +0100

    add select but incomplete

--- js/main.js
+++ js/main.js
@@ -1,32 +1,45 @@
 "use strict"
-function formJobs() {
-    var indicateJobCharge = document.querySelector("#jobdetails-charge");
-    var indicateJobCompany = document.querySelector("#jobdetails-company");
-    var indicateJobDateStart = document.querySelector("#jobdate-start");
-    var indicateJobDateEnd = document.querySelector("#jobdate-end");
-    var indicateJobDescription = document.querySelector("#jobdetails-description");
-    var currentMood = indicateJobCharge.innerHTML;
-    indicateJobCharge.innerHTML= prompt("Cargo en la empresa");
-    var currentMood = indicateJobCompany.innerHTML;
-    indicateJobCompany.innerHTML= prompt("Nombre de la empresa");
-    var currentMood = indicateJobDateStart.innerHTML;
-    indicateJobDateStart.innerHTML= prompt("Fecha de inicio");
-    var currentMood = indicateJobDateEnd.innerHTML;
-    indicateJobDateEnd.innerHTML= prompt("Fecha de final");
-    var currentMood = indicateJobDescription.innerHTML;
-    indicateJobDescription.innerHTML= prompt("Descripción del puesto de trabajo:");
-}
+// function formJobs() {
+//     var indicateJobCharge = document.querySelector("#jobdetails-charge");
+//     var indicateJobCompany = document.querySelector("#jobdetails-company");
+//     var indicateJobDateStart = document.querySelector("#jobdate-start");
+//     var indicateJobDateEnd = document.querySelector("#jobdate-end");
+//     var indicateJobDescription = document.querySelector("#jobdetails-description");
+//     var currentMood = indicateJobCharge.innerHTML;
+//     indicateJobCharge.innerHTML= prompt("Cargo en la empresa");
+//     var currentMood = indicateJobCompany.innerHTML;
+//     indicateJobCompany.innerHTML= prompt("Nombre de la empresa");
+//     var currentMood = indicateJobDateStart.innerHTML;
+//     indicateJobDateStart.innerHTML= prompt("Fecha de inicio");
+//     var currentMood = indicateJobDateEnd.innerHTML;
+//     indicateJobDateEnd.innerHTML= prompt("Fecha de final");
+//     var currentMood = indicateJobDescription.innerHTML;
+//     indicateJobDescription.innerHTML= prompt("Descripción del puesto de trabajo:");
+// }
+
+//PLEGAR Y DESPLEGAR
 function ocultar(item){
 document.querySelector("[id=" + item + "]").style.display = 'none';
 }
-// function closeForm(){
-//
-// document.querySelector(".boxfieldsetjob").style.display = 'none';
-//
-// }
 function mostrar(item){
 document.querySelector("[id=" + item + "]").style.display = 'block';
 }
+
+//SELECT
+
+function yearDesplegable (ano) {
+  var initialYear = 1900;
+  var years = 150;
+  var options = '';
+
+  for (var i=0; i<years; i++) {
+  options = options + '<option value=""' + (initialYear+i) + '>' + (initialYear+i) + '</option>';
+}
+document.querySelector('[id=" + ano + "]').innerHTML = options;
+}
+
+
+
 // var indicateEducationStudies = document.querySelector("#educationdetails-studies");
 // var indicateEducationTypeStudies = document.querySelector("#educationdetails-type-studies");
 // var indicateEducationCentre = document.querySelector("#educationdetails-centre");
