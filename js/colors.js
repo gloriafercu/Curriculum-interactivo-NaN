8e6aa7ebd0280f8a6cfaae10e7c45f175944f4e8
Noeliiap17 <n.parra2003@gmail.com>
2017-12-07 17:30:06 +0100

    add colors box

---
+++ js/colors.js
@@ -0,0 +1,8 @@
+var style1 = document.querySelector('.lateral-1')
+var grey1 = document.querySelector('.principal-1')
+
+var style2 = document.querySelector('.lateral-2')
+var grey2 = document.querySelector('.principal-2')
+
+var style3 = document.querySelector('.lateral-3')
+var grey3 = document.querySelector('.principal-3')
