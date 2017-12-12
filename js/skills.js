56ed5f9c444f22a314471fcb124d59e3ee2de5d7
Noeliiap17 <n.parra2003@gmail.com>
2017-12-12 13:50:51 +0100

    remover functions skills

--- js/skills.js
+++ js/skills.js
@@ -2,31 +2,13 @@
 
 var skill = document.querySelector('#skillstext');
 var skillName = document.querySelector('.skill-name');
-
 var percentage = document.querySelector('#percentage');
 var progress = document.querySelector('.progress-bar__item');
 var barNone = document.querySelector('.progress-bar');
-var buttonAdd = document.querySelector('#plus');
+var buttonAddSkills = document.querySelector('#plus');
 var listSkill = document.querySelector('.skill1');
 var buttonAddDetails = document.querySelector('.add-detail')
 
-
-function fillElement() {
-	skillName.innerHTML += skill.value;
-	return skillName;
-}
-
-function fillPercentage (){
-
-		percentage.value = parseInt(percentage.value);
-  if (percentage.value <= 100 ) {
-    progress.style.width += percentage.value + '%';
-  } else {
-    progress.style.width += '0%';
-  }
-	barNone.classList.remove('hidden');
-	return percentage;
-}
 
 function saveNewSkill() {
 	var addSkill = '<li class="skill1"><p class="skill-name">' + document.getElementById('skillstext').value + '</p><div class="progress-bar"><div class="progress-bar__item" style="width:' +  document.getElementById('percentage').value  + '%"></div></div></li>';
@@ -36,7 +18,6 @@ function saveNewSkill() {
 	document.getElementById('percentage').value = '';
 	}
 
-
 	function saveNewDetails() {
 		var addDetail = '<li>' + document.querySelector('.other-fields').value + '</li>';
 		var newDetail = document.querySelector('.prev-other');
@@ -45,6 +26,5 @@ function saveNewSkill() {
 
 		}
 
-
-buttonAdd.addEventListener('click', saveNewSkill);
+buttonAddSkills.addEventListener('click', saveNewSkill);
 buttonAddDetails.addEventListener('click', saveNewDetails);
