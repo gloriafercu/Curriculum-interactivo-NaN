4bb18dd6a902b7f6c2c448ea5c1eff06527f1f7c
Noeliiap17 <n.parra2003@gmail.com>
2017-12-13 12:06:35 +0100

    add personal dates and extract

--- js/fill-fields.js
+++ js/fill-fields.js
@@ -2,19 +2,38 @@
 
 var camposDatos = document.querySelectorAll('.personal-fields');
 var previewDatos = document.querySelectorAll('.prev-contactdetails');
+var buttonAddJob = document.querySelector('.add-job');
+var btnAddEdu = document.querySelector('.add-edu');
+var btnSavePerson = document.querySelector('.save-personal');
 
 
-function rellenarPersonal() {
-	for (var i = 0; i < previewDatos.length; i++) {
-		previewDatos[i].innerHTML = camposDatos[i].value;
-		console.log("Entro en el for");
-	}
-	return camposDatos.value;
+// RELLENAR Y AÑADIR DATOS PERSONALES
+function saveNewPersonal() {
+	var addPerson = '<li id="nombre" class="prev-contactdetails ">' + document.getElementById('name').value + '</li><li id="apellidos" class="prev-contactdetails">' + document.getElementById('surnames').value + '</li><li id="profesion" class="prev-contactdetails">' + document.getElementById('profession').value + '</li><li id="direccion" class="prev-contactdetails">' + document.getElementById('address').value + '</li><li id="mail" class="prev-contactdetails">' + document.getElementById('email').value +
+	'</li><li id="telefono" class="prev-contactdetails">' + document.getElementById('phone').value + '</li><li class="prev-contactdetails">' + document.getElementById('twitter').value + '</li><li class="prev-contactdetails">' + document.getElementById('linkedin').value + '</li>'
+
+	var newPerson = document.querySelector('.prev-list-personal');
+	newPerson.innerHTML = addPerson;
+	document.getElementById('name').value="";
+	document.getElementById('surnames').value="";
+	document.getElementById('profession').value="";
+	document.getElementById('address').value="";
+	document.getElementById('email').value="";
+	document.getElementById('phone').value="";
+	document.getElementById('twitter').value="";
+	document.getElementById('linkedin').value="";
+}
+
+// RELLENAR Y AÑADIR EXTRACTO
+function saveExtract() {
+	var addExtract ='<p id="resumen" class="prev-contactdetails">' + document.getElementById('summary').value + '</p>';
+
+	var newExtract = document.getElementById('resumen');
+	newExtract.innerHTML = addExtract;
+	document.getElementById('summary').value="";
 }
 
 // RELLENAR Y AÑADIR EXPERIENCIA PROFESIONAL
-
-var buttonAddJob = document.querySelector('.add-job');
 
 function saveNewJobs() {
 	var addJob = '<li class="prev-jobdetails-job"><p class="camps-titles">Puesto de trabajo:</p> <span id="trabajo" class="prev-job">' + document.getElementById('job').value + '</span></li>' + '<li class="prev-jobdetails-charge"><p class="camps-titles">Cargo:</p> <span id="cargo" class="prev-job">' + document.getElementById('job-charge').value+ '</span></li>' + '<li id="jobdetails-company" class="prev-jobdetails-company"><p class="camps-titles">Empresa:</p> <span id="empresa" class="prev-job">' + document.getElementById('job-bussines').value + '</span></li>' + '<li class="prev-jobdetails-date"><ul class="prev-jobdate"><li id="jobdate-start" class="prev-jobdate-start"><p class="camps-titles">Fecha de inicio:</p> <span id="inicio-mes" class="prev-job">' + document.getElementById('job-month-start').value +
@@ -39,19 +58,13 @@ function saveNewJobs() {
 	document.getElementById('job-extract').value="";
 }
 
-
-
-
-
 // RELLENAR Y AÑADIR EDUCACIÓN
 
 
-var btnAddEdu = document.querySelector('.add-edu');
 function addMoreEducation () {
 	var moreEducation = '<li id="educationdetails-studies" class="prev-educationdetails-studies"><p class="camps-titles">Titulación:</p> <span id="titulacion" class="prev-edu">' + document.getElementById('education-studies').value + '</span></li><li id="educationdetails-type-studies" class="prev-educationdetails-type-studies"><p class="camps-titles">Tipo de estudios:</p> <span id="tipo-estudios" class="prev-edu">'+ document.getElementById('education-type-studies').value + '</span></li><li id="educationdetails-centre" class="prev-educationdetails-centre"><p class="camps-titles">Centro de estudios:</p> <span id="centro" class="prev-edu">'+ document.getElementById('education-institution').value +'</span></li><li class="prev-educationdetails-date"><ul class="prev-educationdate"><li id="educationdate-start" class="prev-educationdate-start"><p class="camps-titles">Año de inicio:</p><span>'
 	+ document.getElementById('education-year-start').value +'</span></li><li id="educationdate-end" class="prev-educationdate-end"><p class="camps-titles">Año de finalización:</p><span id="año-fin" class="prev-edu">'
-	 + document.getElementById('education-year-end').value + '</span></li></ul><li id="educationdetails-description" class="prev-educationdetails-description"> <p class="camps-titles">Descripción:</p> <span id="descripcion-estudios" class="prev-edu">' + document.getElementById('education-extract').value + '</span></li><br>'
-
+	 + document.getElementById('education-year-end').value + '</span></li></ul><li id="educationdetails-description" class="prev-educationdetails-description"> <p class="camps-titles">Descripción:</p> <span id="descripcion-estudios" class="prev-edu">' + document.getElementById('education-extract').value + '</span></li><br>';
 
 	var container = document.querySelector('.prev-education');
 	container.innerHTML += moreEducation;
@@ -64,6 +77,7 @@ function addMoreEducation () {
 
 }
 
-
+btnSavePerson.addEventListener('click', saveNewPersonal);
+btnSavePerson.addEventListener('click', saveExtract);
 buttonAddJob.addEventListener('click', saveNewJobs);
 btnAddEdu.addEventListener('click', addMoreEducation);
