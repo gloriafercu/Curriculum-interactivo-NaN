a1ed856722a9a98d4585e9d88c86dfde9274af04
mluena <maluenarod@gmail.com>
2017-12-14 12:50:47 +0100

    adding styles to print preview

--- js/vista.js
+++ js/vista.js
@@ -1,31 +1,29 @@
 'use strict'
 
-/*$(document).ready(function () {
-		$("input#imprime").on('click', function (ev) {
-				CallPrint();
-				clickimprime();
-		});
-});
-function CallPrint() {
-		var divToPrint = document.querySelector('cv');
-		var newWin = window.open('width=100,height=100', '_blank');
-		newWin.focus();
-		newWin.document.open();
-		newWin.document.write(divToPrint.innerHTML);
-		newWin.document.close();
-		setTimeout(function () { newWin.close(); }, 10);
-}
-function clickimprime() {
-		$("preview").click();
-}*/
+function vistaprevia() {
+	var request = new XMLHttpRequest(); //hacemos la petición con AJAX
+	request.open('GET', 'styles/style.css'); // abrimos la pentición (es de tipo GET y le podemos asignar tanto una URL absoluta como relativa)
+	request.addEventListener('load', abrirVistaPrevia); //(evento para que llame a abrirVistaPrevia cuando la petición cargue)
 
-function vistaprevia () {
-	var prtContent = document.getElementById("printcv");
-	var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
-	WinPrint.document.write(prtContent.innerHTML);
-	WinPrint.document.close();
+	function abrirVistaPrevia() {
+		var estilo = request.responseText; //variable que creamos dnd se genera el callback de la petición).
+		var prtContent = document.getElementById("printcv").innerHTML; //variable dnd seleccionamos la sección de HTML a la que queremos aplicar nuestra función.
 
+		var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0'); //generamos una ventana emergente y le damos dimensiones...
+		if (WinPrint != null) {
+			WinPrint.document.write('<html>');
+			WinPrint.document.write('<head><style type="text/css">' + estilo + '</style></head>');
+			WinPrint.document.write('<body>');
+			WinPrint.document.write(prtContent);
+			WinPrint.document.write('</body>');
+			WinPrint.document.write('</html>');
+			WinPrint.document.close();
+		} else {
+			alert('Activa los pop-ups, alma cándida');
+		} // condicional para comprobar si el bloqueo de pop-ups está activado y aviso de desactivarlo en caso afirmativo
+	}
 
+	request.send(); //envío de la petición
 }
 
 
